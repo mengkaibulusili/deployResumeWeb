32826e2237f6c6d1680e836315f7e245cb367cb0
lmk <734449600@qq.com>
2020-10-16 21:22:04 +0800

    更新补丁

--- myapp/build/service-worker.js
+++ myapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2af3e359f9dbf5ff3dc666fc5420f038.js"
+  "/precache-manifest.8043ff2109a7c3e02677a8951518b5f2.js"
 );
 
 self.addEventListener('message', (event) => {
